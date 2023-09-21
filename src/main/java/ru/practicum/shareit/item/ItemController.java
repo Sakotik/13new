c3d87b1cdca4fb1f5fd4f5e8363e79bf09fc91f2
Sakotik <a.shishanovskaya@gmail.com>
2023-09-21 11:31:48 +0300

    14

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,58 +2,66 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.CommentDtoOut;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemDtoOut;
+import ru.practicum.shareit.item.service.ItemServiceImpl;
 
-import javax.validation.constraints.Positive;
+import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/items")
 @RequiredArgsConstructor
-@Slf4j
 public class ItemController {
-    private final ItemService itemService;
-
-    @GetMapping
-    public List<ItemDto> getUserItems(@RequestHeader("X-Sharer-User-Id") @Positive Long userId) {
-        log.info("Начало обработки запроса на получение всех вещей пользователя с id={}", userId);
-        List<ItemDto> itemsDto = itemService.getUserItems(userId);
-        log.info("Завершение обработки запроса на получение всех вещей пользователя с id={}", userId);
-        return itemsDto;
-    }
-
-    @GetMapping("/{itemId}")
-    public ItemDto getItem(@PathVariable @Positive Long itemId) {
-        log.info("Начало обработки запроса на получение вещи с id={}", itemId);
-        ItemDto itemDto = itemService.getItem(itemId);
-        log.info("Завершение обработки запроса на получение вещи с id={}", itemId);
-        return itemDto;
-    }
-
-    @GetMapping("/search")
-    public List<ItemDto> searchItems(@RequestParam String text) {
-        log.info("Начало обработки запроса на поиск вещей по запросу: {}", text);
-        List<ItemDto> itemsDto = itemService.searchItems(text);
-        log.info("Завершение обработки запроса на поиск вещей по запросу: {}", text);
-        return itemsDto;
-    }
+    public static final String USER_HEADER = "X-Sharer-User-Id";
+    private final ItemServiceImpl itemService;
 
     @PostMapping
-    public ItemDto createItem(@RequestBody ItemDto itemDto, @RequestHeader("X-Sharer-User-Id") @Positive Long ownerId) {
-        log.info("Начало обработки запроса на создание вещи: {} пользователем с id={}", itemDto, ownerId);
-        ItemDto createdItemDto = itemService.createItem(itemDto, ownerId);
-        log.info("Завершение обработки запроса на создание вещи: {} пользователем с id={}", itemDto, ownerId);
-        return createdItemDto;
+    public ItemDtoOut add(@RequestHeader(USER_HEADER) Long userId,
+                          @Valid @RequestBody ItemDto itemDto) {
+        log.info("POST Запрос на добавление пользователем с id = {} предмета {}", userId, itemDto.toString());
+        return itemService.add(userId, itemDto);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto updateItem(@RequestBody ItemDto updatedItemDto, @PathVariable @Positive Long itemId,
-                              @RequestHeader("X-Sharer-User-Id") @Positive Long userId) {
-        updatedItemDto.setId(itemId);
-        log.info("Начало обработки запроса на обновление вещи пользователем с id={}: {}", userId, updatedItemDto);
-        ItemDto itemDto = itemService.updateItem(updatedItemDto, userId);
-        log.info("Завершение обработки запроса на обновление вещи пользователем с id={}: {}", userId, updatedItemDto);
-        return itemDto;
+    public ItemDtoOut update(@RequestHeader(USER_HEADER) Long userId,
+                             @RequestBody ItemDto itemDto,
+                             @PathVariable Long itemId) {
+        log.info("PATCH Запрос на обновление предмета с id = {} пользователем с id = {} ", itemId, userId);
+        return itemService.update(userId, itemId, itemDto);
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDtoOut findById(@RequestHeader(USER_HEADER) Long userId,
+                               @PathVariable("itemId")
+                               Long itemId) {
+        log.info("GET Запрос на получение предмета с id = {} пользователем с id = {} ", itemId, userId);
+        return itemService.findItemById(userId, itemId);
+    }
+
+    @GetMapping
+    public List<ItemDtoOut> findAll(@RequestHeader(USER_HEADER) Long userId) {
+        log.info("GET Запрос на получение предметов пользователя с id = {}", userId);
+        return itemService.findAll(userId);
+    }
+
+    @GetMapping("/search")
+    public List<ItemDtoOut> searchItems(@RequestHeader(USER_HEADER) Long userId,
+                                        @RequestParam(name = "text") String text) {
+        log.info("GET Запрос на поиск предметов c текстом = {}", text);
+        return itemService.search(userId, text);
+    }
+
+    @PostMapping("/{itemId}/comment")
+    public CommentDtoOut createComment(@RequestHeader(USER_HEADER) Long userId,
+                                       @Validated @RequestBody CommentDto commentDto,
+                                       @PathVariable Long itemId) {
+        log.info("POST Запрос на создание комментария id = {}", itemId);
+        return itemService.createComment(userId, commentDto, itemId);
     }
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,17 +1,36 @@
 package ru.practicum.shareit.request;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
+import lombok.*;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "requests", schema = "public")
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+    @Column(name = "description", nullable = false, length = 255)
     private String description;
-    @Positive
-    private Long requestor; // id пользователя, создавшего запрос
+
+    @ManyToOne
+    @JoinColumn(name = "requestor_id")
+    @ToString.Exclude
+    private User requester;
+
     private LocalDateTime created;
+
+    public ItemRequest(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
 }
